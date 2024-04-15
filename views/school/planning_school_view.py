9009542e1d91c7292b24a3e872a8e2a958a87324
xuejiajia <xjj1987@qq.com>
2024-04-15 19:23:31 +0800

    '

--- views/school/planning_school_view.py
+++ views/school/planning_school_view.py
@@ -7,12 +7,16 @@ from views.models.school import School
 # from fastapi import Field
 from fastapi import Query
 from pydantic import BaseModel, Field
+from mini_framework.web.std_models.page import PageRequest
 
 
 class PlanningSchoolView(BaseView):
+
     async def get(self,planning_school_no:str= Query(None, title="学校编号", description="学校编号",min_length=1,max_length=20,example='SC2032633'),
                   planning_school_name:str= Query(None, description="学校名称" ,min_length=1,max_length=20,example='XX小学'),
+
                   ):
+
         res = PlanningSchool(
             planning_school_name=planning_school_name,
             planning_school_no=  planning_school_no,
@@ -86,5 +90,63 @@ class PlanningSchoolView(BaseView):
     async def patch(self,planning_school:PlanningSchool):
         print(planning_school)
         return  planning_school
+
+
+
+    async def page(self,page_request: PageRequest,
+                   planning_school_no:str= Query(None, title="学校编号", description="学校编号",min_length=1,max_length=20,example='SC2032633'),
+                  planning_school_name:str= Query(None, description="学校名称" ,min_length=1,max_length=20,example='XX小学'),
+
+
+
+
+                  ):
+
+        res = PlanningSchool(
+            planning_school_name=planning_school_name,
+            planning_school_no=  planning_school_no,
+            planning_school_operation_license_number=planning_school_no,
+            block='',
+            borough='',
+            planning_school_type='中小学',
+            planning_school_operation_type='学前教育',
+
+            planning_school_operation_type_lv2='小学',
+            planning_school_operation_type_lv3='附设小学班',
+            planning_school_org_type='民办',
+            planning_school_level='5',
+            status='正常',
+            planning_school_code='SC562369322SG',
+            kg_level='5',
+            created_uid='1',
+            updated_uid='21',
+            created_at='2021-10-10 00:00:00',
+            updated_at='2021-10-10 00:00:00',
+            deleted='0',
+            planning_school_short_name='MXXX',
+            planning_school_en_name='MinxingPrimarySCHOOL',
+            create_planning_school_date='2021-10-10 00:00:00',
+            social_credit_code='XH423423876867',
+            founder_type='地方',
+            founder_name='上海教育局',
+            founder_code='SC562369322SG',
+            urban_rural_nature='城镇',
+            planning_school_org_form='教学点',
+            planning_school_closure_date='',
+            department_unit_number='SC562369322SG',
+            sy_zones='铁西区',
+            sy_zones_pro='沈阳',
+            primary_planning_school_system='6',
+            primary_planning_school_entry_age='6',
+            junior_middle_planning_school_system='3',
+            junior_middle_planning_school_entry_age='12',
+            senior_middle_planning_school_system='3',
+            historical_evolution='xxxxxxxxxxxxxxxxxxxx',
+
+
+
+        )
+        return  [res]
+
 
 
