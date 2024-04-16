1ab48a5c097ff2871998797d099adb202f4b4bb1
chengjiru <chengjiru@12345>
2024-04-16 14:48:07 +0800

    优化在职员工与新入职员工

--- views/teachers/router.py
+++ views/teachers/router.py
@@ -3,9 +3,11 @@ from mini_framework.web.router import Router
 from views.models.teachers import Teachers
 
 from views.teachers.teachers_view import TeachersView
+from views.teachers.newteachers_view import NewTeachersView
 
 
 def routers():
     router = Router()
-    router.include_api_view_class(TeachersView, "/v1/teachers", response_cls=Teachers, description="教师管理")
+    router.include_api_view_class(TeachersView, "/v1/teachers",  description="在职教师管理")
+    router.include_api_view_class(NewTeachersView, "/v1/newteachers", description="新入职教师管理")
     return router
