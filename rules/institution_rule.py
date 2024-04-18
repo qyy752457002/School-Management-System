d70bc12e9631c35578ffac2d93bfe6d9b93720d1
xuejiajia <xjj1987@qq.com>
2024-04-18 10:19:45 +0800

    各rule调整引入位置/事业单位的字段映射

--- rules/institution_rule.py
+++ rules/institution_rule.py
@@ -114,6 +114,6 @@ class InstitutionRule(object):
         paging = await self.institution_dao.query_institution_with_page(institution_name, institution_id,institution_no,
                                                                                 page_request)
         # 字段映射的示例写法   , {"hash_password": "password"}
-        paging_result = PaginatedResponse.from_paging(paging, InstitutionModel)
+        paging_result = PaginatedResponse.from_paging(paging, InstitutionModel, {"create_institution_date": "create_date","web_url": "website_url",})
         return paging_result
 
