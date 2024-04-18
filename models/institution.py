0c9f39ef6a69524f3aadb75c2cf1cc7f15e8fc1b
xuejiajia <xjj1987@qq.com>
2024-04-18 09:30:36 +0800

    事业单位model/todo

---
+++ models/institution.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import mapped_column, Mapped
+
+from mini_framework.databases.entities import BaseDBModel
+
+
+class Institution(BaseDBModel):
+    """
+    行政事业单位表
+institution_name: str = Field(...,  description="单位名称",examples=['文化部'])
+    institution_en_name: str = Field(...,   description=" 单位名称英文",examples=['CEDUCUL'])
+    institution_category: str = Field(...,   description=" 单位分类",examples=['事业单位'])
+    institution_type: str = Field(...,   description="单位类型 ",examples=[''])
+    institution_code: str = Field(...,   description=" 机构代码",examples=['DKE1865656'])
+    create_date: str = Field(...,   description=" 成立年月",examples=['2020-10-23'])
+    department_unit_number: str = Field(...,   description=" 属地管理行政部门单位号",examples=['DFG454353454'])
+    sy_zones: str = Field(...,   description=" 属地管理行政部门所在地地区",examples=['铁西区'])
+    social_credit_code: str = Field(...,   description=" 统一社会信用代码",examples=['DK156512656'])
+    postal_code: str = Field(...,   description=" 邮政编码",examples=['4587236'])
+urban_rural_nature: str = Field(...,   description=" 城乡性质",examples=['城镇'])
+    location_economic_attribute: str = Field(...,   description=" 所在地经济属性",examples=['镇'])
+    leg_repr_certificatenumber: str = Field(...,   description=" 法人证书号",examples=['DF1256565656'])
+    is_entity: str = Field(...,   description=" 是否实体",examples=['是'])
+    status: str = Field(...,   description=" 状态",examples=[''])
+    membership_no: str = Field(...,   description=" 隶属单位号",examples=['DFF1565165656'])
+    membership_category: str = Field(...,   description=" 隶属单位类型",examples=['行政'])
+
+
+    """
+    __tablename__ = 'lfun_institutions'
+    __table_args__ = {'comment': '行政事业单位表'}
+
+    id: Mapped[int] = mapped_column(primary_key=True, comment="ID",autoincrement=True)
+
+    fax_number: Mapped[str] = mapped_column(String(64), nullable=False, comment="传真电话")
+    email: Mapped[str] = mapped_column(String(64), nullable=False, comment="单位电子信箱")
+
+
+    contact_number: Mapped[str] = mapped_column(String(64), nullable=False, comment="联系电话")
+    area_code: Mapped[str] = mapped_column(String(64), nullable=False, comment="电话区号")
+
+
+    leg_repr_name: Mapped[str] = mapped_column(String(64), nullable=False, comment="法定代表人姓名")
+    party_leader_name: Mapped[str] = mapped_column(String(64), nullable=False, comment="党组织负责人姓名")
+    party_leader_position: Mapped[str] = mapped_column(String(64), nullable=False, comment="党组织负责人职务")
+    adm_leader_name: Mapped[str] = mapped_column(String(64), nullable=False, comment="行政负责人姓名")
+    adm_leader_position: Mapped[str] = mapped_column(String(64), nullable=False, comment="行政负责人职务")
+    postal_code: Mapped[str] = mapped_column(String(64), nullable=False, comment="邮政编码")
+    detailed_address: Mapped[str] = mapped_column(String(64), nullable=False, comment="详细地址")
+    related_license_upload: Mapped[str] = mapped_column(String(64), nullable=False, comment="相关证照上传")
+
+
+    long: Mapped[str] = mapped_column(String(64), nullable=False, comment="所在经度")
+    lat: Mapped[str] = mapped_column(String(64), nullable=False, comment="所在纬度")
+    web_url: Mapped[str] = mapped_column(String(64), nullable=False, comment="网址")
+
+    # loc_area: Mapped[str] = mapped_column(String(64), nullable=False, comment="园所所在地区")
+
+    # loc_area_pro: Mapped[str] = mapped_column(String(64), nullable=False, comment="园所所在地(省级)")
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, comment="创建时间")
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, comment="更新时间")
+
+    created_uid: Mapped[int] = mapped_column(  nullable=True , comment="创建人",default=0)
+    updated_uid: Mapped[int] = mapped_column( nullable=True , comment="操作人",default=0)
+    deleted: Mapped[int] = mapped_column( nullable=True  , comment="删除态",default=0)
+
