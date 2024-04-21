0483e964ca2d7f45a09787689ec54dcd9102df26
xuejiajia <xjj1987@qq.com>
2024-04-21 14:00:39 +0800

    调整年级的下拉搜索/年级OK

--- views/models/grades.py
+++ views/models/grades.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 
 class Grades(BaseModel):
-    school_id: int = Field(..., title="学校ID", description="学校ID",examples=[''])
+    school_id: int = Field(0, title="学校ID", description="学校ID",examples=[''])
     grade_no: str = Field(..., title="年级编号", description="年级编号",examples=['一年级'])
     grade_name: str = Field(..., title="Grade_name",description="Grade_name",examples=['一年级'])
     grade_alias: str = Field(...,  description="Grade_alias",examples=['一年级'])
