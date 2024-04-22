f84f900466bc94d65ac08a3161781f038d9d65ea
xuejiajia <xjj1987@qq.com>
2024-04-22 15:36:21 +0800

    导入城市

---
+++ sql/lfun_enum_value_202404221531_prov.sql
@@ -0,0 +1,70 @@
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (2,'founder_type','regional','地方',NULL,'0',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (8,'founder_type_lv2','jointVentureWithLegalPersonStatus','具有法人资格的中外合作办',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (4,'founder_type_lv2','private','民办',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (6,'founder_type_lv3','private','民办',NULL,'4',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (10,'founder_type_lv2','collective','集体',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (12,'founder_type_lv2','military','部队',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (14,'founder_type_lv2','publicInstitution','事业单位',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (25,'founder_type_lv3','publicInstitution','事业单位',NULL,'5',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (26,'founder_type_lv3','publicInstitution','事业单位',NULL,'5',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (27,'founder_type_lv2','regionalEnterprise','地方企业',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false);
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (28,'founder_type_lv2','otherDepartments','其他部门',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (29,'founder_type_lv2','educationDepartments','教育部门',NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (3,'founder_type','central','中央',NULL,'0',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (23,'founder_type_lv3','centralOtherDepartments','中央其他部门',NULL,'5',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (9,'founder_type_lv3','jointVentureWithLegalPersonStatus','具有法人资格的中外合作办',NULL,'8',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (11,'founder_type_lv3','collective','集体',NULL,'10',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (13,'founder_type_lv3','military','部队',NULL,'12',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (15,'founder_type_lv3','publicInstitution','事业单位',NULL,'14',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (16,'founder_type_lv3','regionalEnterprise','地方企业',NULL,'27',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (17,'founder_type_lv3','countyLevelOtherDepartments','县级其他部门',NULL,'28',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false);
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (18,'founder_type_lv3','districtLevelOtherDepartments','地级其他部门',NULL,'28',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (19,'founder_type_lv3','provincialLevelOtherDepartments','省级其他部门',NULL,'28',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (20,'founder_type_lv3','countyLevelEducationDepartments','县级教育部门',NULL,'29',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (21,'founder_type_lv3','districtLevelEducationDepartments','地级教育部门',NULL,'29',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (22,'founder_type_lv3','provincialLevelEducationDepartments','省级教育部门',NULL,'29',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (30,'founder_type_lv2','centralOtherDepartments','中央其他部门',NULL,'3',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (24,'founder_type_lv3','centralEducationDepartment','教育部门',NULL,'3',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (38,'founder_type_lv3','centralOtherDepartments','中央其他部门',NULL,'30',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (39,'founder_type_lv3','centralEducationDepartment','中央教育部门',NULL,'24',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
+	 (42,'province','110000','北京市',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false);
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (43,'province','120000','天津市',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (44,'province','130000','河北省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (45,'province','140000','山西省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (46,'province','150000','内蒙古自治区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (47,'province','210000','辽宁省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (48,'province','220000','吉林省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (49,'province','230000','黑龙江省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (50,'province','310000','上海市',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (51,'province','320000','江苏省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (52,'province','330000','浙江省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false);
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (53,'province','340000','安徽省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (54,'province','350000','福建省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (55,'province','360000','江西省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (56,'province','370000','山东省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (57,'province','410000','河南省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (58,'province','420000','湖北省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (59,'province','430000','湖南省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (60,'province','440000','广东省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (61,'province','450000','广西壮族自治区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (62,'province','460000','海南省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false);
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (63,'province','500000','重庆市',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (64,'province','510000','四川省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (65,'province','520000','贵州省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (66,'province','530000','云南省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (67,'province','540000','西藏自治区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (68,'province','610000','陕西省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (69,'province','620000','甘肃省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (70,'province','630000','青海省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (71,'province','640000','宁夏回族自治区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (72,'province','650000','新疆维吾尔自治区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false);
+INSERT INTO public.lfun_enum_value (id,enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
+	 (73,'province','710000','台湾省',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (74,'province','810000','香港特别行政区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false),
+	 (75,'province','820000','澳门特别行政区',NULL,NULL,true,NULL,NULL,'2020-10-01 00:00:00','2020-10-01 00:00:00',false);
