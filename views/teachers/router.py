4698527bea3de9ff3958966aaf9b4a9e6e3b4c14
chengjiru <chengjiru@12345>
2024-04-22 14:51:02 +0800

    修改页面描述

--- views/teachers/router.py
+++ views/teachers/router.py
@@ -15,12 +15,12 @@ def routers():
     router.include_api_view_class(NewTeachersView, "/v1/new-teachers", description="新入职教师管理")
     router.include_api_view_class(TeacherLearnExperienceView, "/v1/teacher-extend", description="教师学习经历信息管理")
     router.include_api_view_class(TeacherWorkExperienceView, "/v1/teacher-extend", description="教师工作经历信息管理")
-    router.include_api_view_class(TeacherJobAppointmentsView, "/v1/teacher-extend", description="教师任职信息管理")
-    router.include_api_view_class(TeacherProfessionalTitlesView, "/v1/teacher-extend", description="教师职称信息管理")
+    router.include_api_view_class(TeacherJobAppointmentsView, "/v1/teacher-extend", description="教师岗位任聘管理")
+    router.include_api_view_class(TeacherProfessionalTitlesView, "/v1/teacher-extend", description="教师专业技术职位信息管理")
     router.include_api_view_class(TeacherQualificationsView, "/v1/teacher-extend", description="教师资格信息管理")
     router.include_api_view_class(TeacherSkillCertificatesView, "/v1/teacher-extend",
                                   description="教师技能证书信息管理")
-    router.include_api_view_class(TeacherEthicRecordsView, "/v1/teacher-extend", description="教师道德记录信息管理")
+    router.include_api_view_class(TeacherEthicRecordsView, "/v1/teacher-extend", description="教师师德记录信息管理")
     router.include_api_view_class(EducationalTeachingView, "/v1/teacher-extend", description="教师教育教学信息管理")
 
     return router
