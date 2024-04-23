4039cc736f95b6a5283fadc6747566e8da0acd48
xuejiajia <xjj1987@qq.com>
2024-04-23 17:23:19 +0800

    班级分页的参数 筛选/

--- daos/class_dao.py
+++ daos/class_dao.py
@@ -14,7 +14,6 @@ class ClassesDAO(DAOBase):
         result = await session.execute(select(Classes).where(Classes.id == classes_id))
         return result.scalar_one_or_none()
 
-
     async def get_classes_by_classes_name(self, classes_name):
         session = await self.slave_db()
         result = await session.execute(
@@ -28,78 +27,72 @@ class ClassesDAO(DAOBase):
         await session.refresh(classes)
         return classes
 
-    async def update_classes(self, classes,ctype=1):
+    async def update_classes(self, classes, ctype=1):
         session = await self.master_db()
         # session.add(classes)
         if ctype == 1:
             update_stmt = update(Classes).where(Classes.id == classes.id).values(
-                classes_id= classes.classes_id,
-                grade_id= classes.grade_id,
-                grade_no= classes.grade_no,
-                is_att_class= classes.is_att_class,
-                att_class_type= classes.att_class_type,
-                class_name= classes.class_name,
-                class_number= classes.class_number,
-                year_established= classes.year_established,
-                teacher_id_card= classes.teacher_id_card,
-                teacher_name= classes.teacher_name,
-                education_stage= classes.education_stage,
-                classes_system= classes.classes_system,
-                monitor= classes.monitor,
-                class_type= classes.class_type,
-                is_bilingual_class= classes.is_bilingual_class,
-                major_for_vocational= classes.major_for_vocational,
-                bilingual_teaching_mode= classes.bilingual_teaching_mode,
-                ethnic_language= classes.ethnic_language,
-
-
+                classes_id=classes.classes_id,
+                grade_id=classes.grade_id,
+                grade_no=classes.grade_no,
+                is_att_class=classes.is_att_class,
+                att_class_type=classes.att_class_type,
+                class_name=classes.class_name,
+                class_number=classes.class_number,
+                year_established=classes.year_established,
+                teacher_id_card=classes.teacher_id_card,
+                teacher_name=classes.teacher_name,
+                education_stage=classes.education_stage,
+                classes_system=classes.classes_system,
+                monitor=classes.monitor,
+                class_type=classes.class_type,
+                is_bilingual_class=classes.is_bilingual_class,
+                major_for_vocational=classes.major_for_vocational,
+                bilingual_teaching_mode=classes.bilingual_teaching_mode,
+                ethnic_language=classes.ethnic_language,
 
             )
         else:
             pass
 
-
         await session.execute(update_stmt)
         await session.commit()
         return classes
 
-
     async def softdelete_classes(self, classes):
         session = await self.master_db()
-        deleted_status= True
+        deleted_status = True
         update_stmt = update(Classes).where(Classes.id == classes.id).values(
-            is_deleted= deleted_status,
+            is_deleted=deleted_status,
         )
         await session.execute(update_stmt)
         # await session.delete(classes)
         await session.commit()
         return classes
 
-
     async def get_classes_count(self):
         session = await self.slave_db()
         result = await session.execute(select(func.count()).select_from(Classes))
         return result.scalar()
 
-    async def query_classes_with_page(self, classes_name, classes_id, classes_no,
-                                              page_request: PageRequest) -> Paging:
+    async def query_classes_with_page(self, borough, block, school_id, grade_id, class_name,
+                                      page_request: PageRequest) -> Paging:
         query = select(Classes)
-        if classes_name:
-            # query = query.where(Classes.classes_name == classes_name)
+        if school_id:
+            query = query.where(Classes.school_id == school_id)
             pass
-        if classes_id:
-            query = query.where(Classes.id == classes_id)
-        if classes_no:
-            # query = query.where(Classes.classes_no == classes_no)
-            pass
+        if grade_id:
+            query = query.where(Classes.grade_id == grade_id)
+        if class_name:
+            # query = query.where(Classes.classes_no == class_name)
+            query = query.where(Classes.class_name.like(f'%{class_name}%'))
+
+            # pass
         paging = await self.query_page(query, page_request)
         return paging
 
-
-
     async def update_classes_byargs(self, classes: Classes, *args, is_commit: bool = True):
-        session =await self.master_db()
+        session = await self.master_db()
         update_contents = get_update_contents(classes, *args)
         query = update(Classes).where(Classes.id == classes.id).values(**update_contents)
         return await self.update(session, query, classes, update_contents, is_commit=is_commit)
-
