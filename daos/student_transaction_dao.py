072528c35783502c9644d9067c23e0f5cda58754
xuejiajia <xjj1987@qq.com>
2024-04-24 14:06:42 +0800

    转学联表倒序

--- daos/student_transaction_dao.py
+++ daos/student_transaction_dao.py
@@ -4,6 +4,7 @@ from mini_framework.databases.queries.pages import Paging
 from mini_framework.web.std_models.page import PageRequest
 
 from models.student_transaction import StudentTransaction
+from models.students import Student
 
 
 class StudentTransactionDAO(DAOBase):
@@ -31,7 +32,9 @@ class StudentTransactionDAO(DAOBase):
 		return result.scalar_one_or_none()
 
 	async def query_studenttransaction_with_page(self, page_request: PageRequest, **kwargs,):
-		query = select(StudentTransaction)
+		query = select(StudentTransaction).select_from(  StudentTransaction).join(Student, StudentTransaction.student_id == Student.student_id )
+		query = query.order_by(StudentTransaction.id.desc())
+
 		for key, value in kwargs.items():
 		   query = query.where(getattr(StudentTransaction, key) == value)
 		paging = await self.query_page(query, page_request)
