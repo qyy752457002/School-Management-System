47714018be8a0c49ec7d2378978eed809f36b536
xuejiajia <xjj1987@qq.com>
2024-04-25 15:40:38 +0800

    '

--- views/models/planning_school.py
+++ views/models/planning_school.py
@@ -150,11 +150,11 @@ class PlanningSchoolBaseInfoOptional(BaseModel):
 class PlanningSchoolKeyAddInfo(BaseModel):
     id:int= Query(None, title="", description="规划校id", example='1'),
 
-    planning_school_name: str = Field(..., title="学校名称", description="1-20字符",examples=['XX小学'])
+    planning_school_name: str = Field(..., title="学校名称", description="学校名称", min_length=1, max_length=30,examples=['XX小学'])
     planning_school_no:str= Query(None, title="学校编号", description="学校编号/园所代码",min_length=1,max_length=20,example='SC2032633'),
-    planning_school_code: str = Field('', title="", description=" 园所标识码",examples=['SC562369322SG'])
-    borough:str=Query(..., title=" Author Email", description=" 行政管辖区",examples=['铁西区']),
-    block: str = Query(..., title=" Author", description="地域管辖区",examples=['铁西区']),
+    planning_school_code: str = Field('', title="", description=" 园所标识码",examples=['SC562369322SG'],min_length=1,max_length=30)
+    borough:str=Query(..., title=" Author Email", description=" 行政管辖区",examples=['铁西区'],min_length=1,max_length=30),
+    block: str = Query(..., title=" Author", description="地域管辖区",examples=['铁西区'],min_length=1,max_length=30),
     planning_school_type: str = Query(..., title="", description=" 学校类型",examples=['中小学']),
     planning_school_operation_type: str = Query(..., title="", description="办学类型/学校性质",examples=['学前教育']),
     planning_school_operation_type_lv2: str = Query(..., title="", description=" 办学类型二级",examples=['小学']),
