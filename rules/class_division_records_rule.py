86ea760ae5cdfe38ad0beea4a188b82a2a7956d4
xuejiajia <xjj1987@qq.com>
2024-04-26 15:18:06 +0800

    分班的 姓名 年级 学校ID写入

--- rules/class_division_records_rule.py
+++ rules/class_division_records_rule.py
@@ -3,15 +3,26 @@ from mini_framework.web.toolkit.model_utilities import orm_model_to_view_model, 
 
 from mini_framework.design_patterns.depend_inject import dataclass_inject
 from mini_framework.web.std_models.page import PaginatedResponse, PageRequest
+
+from daos.class_dao import ClassesDAO
 from daos.class_division_records_dao import ClassDivisionRecordsDAO
+from daos.grade_dao import GradeDAO
+from daos.students_dao import StudentsDao
 from models.class_division_records import ClassDivisionRecords
 from views.common.common_view import page_none_deal
 from views.models.class_division_records import ClassDivisionRecords as ClassDivisionRecordsModel
 from views.models.class_division_records import ClassDivisionRecordsSearchRes
+from views.models.classes import Classes
+from views.models.grades import Grades
+from views.models.students import NewStudentsQuery
+
 
 @dataclass_inject
 class ClassDivisionRecordsRule(object):
     class_division_records_dao: ClassDivisionRecordsDAO
+    student_dao: StudentsDao
+    class_dao: ClassesDAO
+    garde_dao: GradeDAO
 
     async def get_class_division_records_by_id(self, class_division_records_id):
         class_division_records_db = await self.class_division_records_dao.get_class_division_records_by_id(class_division_records_id)
@@ -28,10 +39,34 @@ class ClassDivisionRecordsRule(object):
         for student_id in student_ids:
             class_division_records = ClassDivisionRecordsModel(class_id=class_id, student_id=student_id)
             class_division_records_db = view_model_to_orm_model(class_division_records, ClassDivisionRecords, exclude=["id"])
+
+            #     读取 班级细腻系  学生信息
+            # class_info = await self.class_dao.get_classes_info_by_id(class_id)
+            # print(class_info)
+            #
+            # class_info = orm_model_to_view_model(class_info, Classes)
+            #
+            # print(class_info)
+            # class_division_records.grade_id = class_info.grade_id
+            # class_division_records.school_id = class_info.school_id
+            stu_info = await self.student_dao.get_students_by_id(student_id)
+            stu_info = orm_model_to_view_model(stu_info, NewStudentsQuery)
+
+            # stu_info =
+            class_info = orm_model_to_view_model(await self.class_dao.get_classes_by_id(class_id), Classes)
+
+            grade_info = orm_model_to_view_model(await self.garde_dao.get_grade_by_id(class_info.grade_id), Grades)
+
+
+            # class_division_records.student_no = stu_info.student_no
+            class_division_records_db.student_name = stu_info.student_name
+            class_division_records_db.grade_id = class_info.grade_id
+
+            class_division_records_db.school_id = grade_info.school_id
+            # class_division_records_db.status =
+
             class_division_records_db = await self.class_division_records_dao.add_class_division_records(class_division_records_db)
             class_division_records = orm_model_to_view_model(class_division_records_db, ClassDivisionRecordsModel, exclude=["created_at", 'updated_at'])
-
-
 
 
         #
