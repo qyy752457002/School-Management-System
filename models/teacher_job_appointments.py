bae64b9af91d16d4dc8d0caf82da34f0ffdf9136
chengjiru <chengjiru@12345>
2024-04-26 17:23:19 +0800

    老师扩展信息中的job管理增加形式

--- models/teacher_job_appointments.py
+++ models/teacher_job_appointments.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import mapped_column, Mapped
 from mini_framework.databases.entities import BaseDBModel
 from datetime import date
 
+
 class TeacherJobAppointments(BaseDBModel):
     """
     teacher_job_appointments：teacher_job_appointments_id
@@ -26,13 +27,11 @@ class TeacherJobAppointments(BaseDBModel):
     position_category: Mapped[str] = mapped_column(String(64), nullable=False, comment="岗位类别")
     position_level: Mapped[str] = mapped_column(String(64), nullable=False, comment="岗位等级")
     school_level_position: Mapped[str] = mapped_column(String(64), nullable=False, comment="校级职务")
-    is_concurrent_other_positions: Mapped[bool] = mapped_column(default=False, comment="是否兼任其他岗位")
-    concurrent_position_category: Mapped[str] = mapped_column(String(64), nullable=False, comment="兼任岗位类别")
-    concurrent_position_level: Mapped[str] = mapped_column(String(64), nullable=False, comment="兼任岗位登记")
+    is_concurrent_other_positions: Mapped[str] = mapped_column(nullable=False, comment="是否兼任其他岗位")
+    concurrent_position: Mapped[str] = mapped_column(String(255), nullable=True, comment="兼任岗位类别")
     employment_institution_name: Mapped[str] = mapped_column(String(64), nullable=False, comment="任职单位名称")
     appointment_start_date: Mapped[date] = mapped_column(Date, nullable=False, comment="聘任开始时间")
     start_date: Mapped[date] = mapped_column(Date, nullable=True, comment="任职开始年月时间")
     is_deleted: Mapped[bool] = mapped_column(default=False, comment="是否删除")
     approval_status: Mapped[str] = mapped_column(String(64), nullable=False, comment="审批状态",
                                                  default="submitting")
-    
--- rules/teacher_job_appointments_rule.py
+++ rules/teacher_job_appointments_rule.py
@@ -6,6 +6,7 @@ from daos.teachers_dao import TeachersDao
 from models.teacher_job_appointments import TeacherJobAppointments
 from views.models.teacher_extend import TeacherJobAppointmentsModel, TeacherJobAppointmentsUpdateModel
 from business_exceptions.teacher import TeacherNotFoundError, TeacherJobAppointmentsNotFoundError
+import json
 
 
 @dataclass_inject
@@ -16,17 +17,25 @@ class TeacherJobAppointmentsRule(object):
     async def get_teacher_job_appointments_by_teacher_job_appointments_id(self, teacher_job_appointments_id):
         teacher_job_appointments_db = await self.teacher_job_appointments_dao.get_teacher_job_appointments_by_teacher_job_appointments_id(
             teacher_job_appointments_id)
-        teacher_job_appointments = orm_model_to_view_model(teacher_job_appointments_db, TeacherJobAppointmentsModel)
+        teacher_job_appointments = orm_model_to_view_model(teacher_job_appointments_db,
+                                                           TeacherJobAppointmentsUpdateModel,
+                                                           exclude=["concurrent_position"])
+        teacher_job_appointments.concurrent_position = json.loads(teacher_job_appointments_db.concurrent_position)
         return teacher_job_appointments
 
     async def add_teacher_job_appointments(self, teacher_job_appointments: TeacherJobAppointmentsModel):
         exits_teacher = await self.teachers_dao.get_teachers_by_id(teacher_job_appointments.teacher_id)
         if not exits_teacher:
             raise TeacherNotFoundError()
-        teacher_job_appointments_db = view_model_to_orm_model(teacher_job_appointments, TeacherJobAppointments)
+        concurrent_positions_json = json.dumps(teacher_job_appointments.concurrent_position)
+        teacher_job_appointments_db = view_model_to_orm_model(teacher_job_appointments, TeacherJobAppointments,
+                                                              exclude=["concurrent_position"])
+        teacher_job_appointments_db.concurrent_position = concurrent_positions_json
         teacher_job_appointments_db = await self.teacher_job_appointments_dao.add_teacher_job_appointments(
             teacher_job_appointments_db)
-        teacher_job_appointments = orm_model_to_view_model(teacher_job_appointments_db, TeacherJobAppointmentsModel)
+        teacher_job_appointments = orm_model_to_view_model(teacher_job_appointments_db, TeacherJobAppointmentsModel,
+                                                           exclude=["concurrent_position"])
+        teacher_job_appointments.concurrent_position = json.loads(teacher_job_appointments_db.concurrent_position)
         return teacher_job_appointments
 
     async def delete_teacher_job_appointments(self, teacher_job_appointments_id):
@@ -37,7 +46,8 @@ class TeacherJobAppointmentsRule(object):
         teacher_job_appointments_db = await self.teacher_job_appointments_dao.delete_teacher_job_appointments(
             exists_teacher_job_appointments)
         teacher_job_appointments = orm_model_to_view_model(teacher_job_appointments_db, TeacherJobAppointmentsModel,
-                                                           exclude=[""])
+                                                           exclude=["TeacherJobAppointmentsUpdateModel"])
+        teacher_job_appointments.concurrent_position = json.loads(teacher_job_appointments_db.concurrent_position)
         return teacher_job_appointments
 
     async def update_teacher_job_appointments(self, teacher_job_appointments: TeacherJobAppointmentsUpdateModel):
@@ -49,8 +59,10 @@ class TeacherJobAppointmentsRule(object):
         for key, value in teacher_job_appointments.dict().items():
             if value:
                 need_update_list.append(key)
+        teacher_job_appointments.concurrent_position = json.dumps(teacher_job_appointments.concurrent_position)
         teacher_job_appointments = await self.teacher_job_appointments_dao.update_teacher_job_appointments(
             teacher_job_appointments, *need_update_list)
+        teacher_job_appointments.concurrent_position = json.loads(teacher_job_appointments.concurrent_position)
         return teacher_job_appointments
 
     async def get_all_teacher_job_appointments(self, teacher_id):
@@ -60,8 +72,10 @@ class TeacherJobAppointmentsRule(object):
             raise TeacherNotFoundError()
         teacher_job_appointments = []
         for teacher_job_appointment in teacher_job_appointments_db:
-            teacher_job_appointments.append(
-                orm_model_to_view_model(teacher_job_appointment, TeacherJobAppointmentsUpdateModel))
+            item = orm_model_to_view_model(teacher_job_appointment, TeacherJobAppointmentsUpdateModel,
+                                           exclude=["concurrent_position"])
+            item.concurrent_position = json.loads(teacher_job_appointment.concurrent_position)
+            teacher_job_appointments.append(item)
         return teacher_job_appointments
 
     async def submitting(self, teacher_job_appointments_id):
