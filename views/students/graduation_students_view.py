81bfcd64363f98628e4c3ed7f834fca8700f5491
xuejiajia <xjj1987@qq.com>
2024-04-28 13:42:54 +0800

    毕业生列表加行政区筛选

--- views/students/graduation_students_view.py
+++ views/students/graduation_students_view.py
@@ -32,14 +32,14 @@ class GraduationStudentsView(BaseView):
                    student_gender: str = Query(None, title="性别", description="性别", example="Male"),
                    edu_number: str = Query(None, title="", description="学籍号码", example=""),
                    class_id: str = Query(None, title="", description="班级", example=""),
-
+                   borough: str = Query('', title="", description="行政属地", example=""),
                    page_request=Depends(PageRequest)):
         print(page_request)
         items = []
 
         res = await self.graduation_student_rule.query_graduation_student_with_page(page_request, student_name,
                                                                                     school_id, student_gender,
-                                                                                    edu_number, class_id)
+                                                                                    edu_number, class_id,borough)
         return res
 
         # return PaginatedResponse(has_next=True, has_prev=True, page=page_request.page, pages=10,
