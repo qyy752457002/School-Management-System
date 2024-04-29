f57cbf89e646806c0ba02726b3f5be254bc5c53d
xuejiajia <xjj1987@qq.com>
2024-04-29 13:37:20 +0800

    校内异动的dao  rule   view

---
+++ daos/student_inner_transaction_dao.py
@@ -0,0 +1,45 @@
+from sqlalchemy import select, func, update
+from mini_framework.databases.entities.dao_base import DAOBase, get_update_contents
+from mini_framework.databases.queries.pages import Paging
+from mini_framework.web.std_models.page import PageRequest
+
+from models.student_inner_transaction import StudentInnerTransaction
+
+
+class StudentInnerTransactionDAO(DAOBase):
+
+	async def add_student_inner_transaction(self, student_inner_transaction: StudentInnerTransaction):
+		session = await self.master_db()
+		session.add(student_inner_transaction)
+		await session.commit()
+		await session.refresh(student_inner_transaction)
+		return student_inner_transaction
+
+	async def get_student_inner_transaction_count(self, ):
+		session = await self.slave_db()
+		result = await session.execute(select(func.count()).select_from(StudentInnerTransaction))
+		return result.scalar()
+
+	async def delete_student_inner_transaction(self, student_inner_transaction: StudentInnerTransaction):
+		session = await self.master_db()
+		await session.delete(student_inner_transaction)
+		await session.commit()
+
+	async def get_student_inner_transaction_by_id(self, id):
+		session = await self.slave_db()
+		result = await session.execute(select(StudentInnerTransaction).where(StudentInnerTransaction.id == id))
+		return result.scalar_one_or_none()
+
+	async def query_student_inner_transaction_with_page(self, pageQueryModel, page_request: PageRequest):
+		query = select(StudentInnerTransaction)
+		
+		### 此处填写查询条件
+		
+		paging = await self.query_page(query, page_request)
+		return paging
+
+	async def update_student_inner_transaction(self, student_inner_transaction, *args, is_commit=True):
+		session = await self.master_db()
+		update_contents = get_update_contents(student_inner_transaction, *args)
+		query = update(StudentInnerTransaction).where(StudentInnerTransaction.id == student_inner_transaction.id).values(**update_contents)
+		return await self.update(session, query, student_inner_transaction, update_contents, is_commit=is_commit)
--- views/models/student_inner_transaction.py
+++ views/models/student_inner_transaction.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import Query
 from pydantic import BaseModel, Field
@@ -8,7 +9,7 @@ from models.student_transaction import AuditAction
 
 class StudentInnerTransactionRes(BaseModel):
     """
-    学籍号  性别 班级  学生状态  行政属地 学校  提交时间
+
     """
     id: int = Query(None, title="", description="id", example='1'),
     student_name: str = Field('', title="", description="姓名", examples=[''])
@@ -16,14 +17,14 @@ class StudentInnerTransactionRes(BaseModel):
     transaction_reason: str = Field('', title="", description="异动原因", examples=[''])
 
     school_name: str = Query(..., title="", description="学校名称", examples=["XXxiaoxue"])
-
     classes: str = Query(..., title="", description="班级", examples=["二2班"])
-
     status: str = Query('', description="", min_length=1, max_length=20, examples=["..."]),
-    # apply_user: str = Query('', title="", description="", examples=["申请人"])
-    # apply_time: str = Query('', title="", description="", examples=["申请时间"])
     student_gender: str = Query('', title="", description="", examples=[""])
-
+    edu_number: str = Field('', title="", description="学籍号码")
+    transaction_time: str = Field('', title="", description="提交时间")
+    class_name: str = Field('', title="Grade_name", description="班级名称", examples=['一年级'])
+    borough: str = Field('', title=" Author Email", description=" 行政管辖区", examples=['铁西区'])
+    approval_status: Optional[str] = Query(None, title="", description="学生状态")
 
 
 class StudentInnerTransaction(BaseModel):
@@ -33,3 +34,12 @@ class StudentInnerTransaction(BaseModel):
     transaction_reason: str = Field('', title="", description="异动原因", examples=[''])
 
 
+class StudentInnerTransactionSearch(BaseModel):
+    """
+    """
+    borough: str = Field('', title=" ", description=" 行政管辖区", examples=['铁西区'])
+    school_id: int  = Query( 0, title="", description="学校名称", examples=["XXxiaoxue"])
+    student_name: str = Field('', title="", description="姓名", examples=[''])
+    student_gender: str = Query('', title="", description="", examples=[""])
+    edu_number: str = Field('', title="", description="学籍号码")
+    class_id: int  = Query( 0, title="", description="班级", examples=["二2班"])
