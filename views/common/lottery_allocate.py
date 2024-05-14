ad9057d9c8c0d863fa99ddc02fdcfb8f30c69bc7
xuejiajia <xjj1987@qq.com>
2024-05-14 13:55:52 +0800

    类的方式来实现 分配

--- views/common/lottery_allocate.py
+++ views/common/lottery_allocate.py
@@ -1,6 +1,13 @@
 import pprint
 import random
 
+class Student:
+    def __init__(self,id:int,name:str,gender:str,score:int,class_id:int):
+        self.id = id
+        self.name = name
+        self.gender = gender
+        self.score = score
+        self.class_id = class_id
 
 class LotteryAllocateTempClass:
     students = []
@@ -8,44 +15,73 @@ class LotteryAllocateTempClass:
     total_score = 0
     gender_percent = 0
     total_student_num=0
+    total_male_num=0
+    total_female_num=0
     is_full = False
 
-    def __init__(self,id:int  ):
+    def __init__(self,id:int ,capacity:int ):
         self.id = id
+        self.class_capacity = capacity
         pass
-    def allocate(self,students):
+    def allocate(self,student):
+        if self.is_full:
+            return False
+        self.students.append(student)
+        self.total_student_num+=1
+        self.total_score+=student.score
+        # self.gender_percent+=student.gender_percent
+        if self.total_student_num == self.class_capacity:
+            self.is_full = True
+        self.total_male_num=0
+        self.total_female_num=0
+        for i,v in enumerate(self.students):
+            if v.gender == '男':
+                self.total_male_num+=1
+            else:
+                self.total_female_num+=1
+                # v.class_id = self.id
+        self.gender_percent=self.total_male_num/self.total_female_num if self.total_female_num>0 else self.total_male_num
+        return True
+
         pass
 
 
 
 def get_min_total_student_num( classes):
-    # tl = [ i for i in classes ]
-    stu_numbers = map(lambda x: x.total_student_num, classes)
+    stu_numbers=[]
+    for i,v in classes.items():
+        stu_numbers.append(v.total_student_num)
+    print(stu_numbers)
     min_total_student_num=min(stu_numbers)
-    for i,v in enumerate(stu_numbers):
-        if v == min_total_student_num:
-            return i
-
-    return 1
+    res=[]
+    for i,v in classes.items():
+        if v.total_student_num == min_total_student_num:
+            res.append(i)
+    return res
 
 def get_min_gender_percent( classes):
-    # tl = [ i for i in classes ]
-    stu_numbers = map(lambda x: x.gender_percent, classes)
+    stu_numbers=[]
+    for i,v in classes.items():
+        stu_numbers.append(v.gender_percent)
+    # stu_numbers = map(lambda x: x.gender_percent, classes)
     min_total_student_num=min(stu_numbers)
-    for i,v in enumerate(stu_numbers):
-        if v == min_total_student_num:
-            return i
+    res=[]
 
-    return 1
+    for i,v in classes.items():
+        if v.gender_percent == min_total_student_num:
+            res.append(i)
+
+    return res
+
 
 
 def main():
 
     #  打乱顺序   性别均衡  成绩均衡  年龄 生日  小学  初中要素包含 来源学校小学/ 乡镇
     # 假设我们有一个学生列表，每个学生有一个唯一的ID
-    student_total = 20   #总学生数
+    student_total = 15   #总学生数
     # 班级数量
-    num_classes = 2
+    num_classes = 3
     classes_capacity = 10  #班级人数上限
     students = list(range(1, student_total+1))  # 假设有个学生
     # studic = dict(
@@ -54,8 +90,9 @@ def main():
     for i in students:
         tt = random.randint(0, 1)
         score = random.randint(1, 100)
-        stu={"id":i ,"gender":'女' if tt==0 else '男' ,"score":score
+        stutt={"id":i ,"gender":'女' if tt==0 else '男' ,"score":score
         }
+        stu= Student(i,'',stutt['gender'],stutt['score'],0)
         studic.append(stu)
 
     pprint.pprint(studic)
@@ -85,7 +122,7 @@ def main():
 
 
     # 初始化班级字典
-    classes = {i: LotteryAllocateTempClass(i) for i in range(1, num_classes + 1)}
+    classes = {i: LotteryAllocateTempClass(i,classes_capacity) for i in range(1, num_classes + 1)}
     # classes_total = {i: 0 for i in range(1, num_classes + 1)}
     # classes_male = {i: 0 for i in range(1, num_classes + 1)}
     # classes_female = {i: 0 for i in range(1, num_classes + 1)}
@@ -100,11 +137,30 @@ def main():
 
         min_value_total = get_min_total_student_num(classes)
         min_value_gender_percent = get_min_total_student_num(classes)
+        s1= set(min_value_total)
+        s2= set(min_value_gender_percent)
+        com = s2 & s1
+        if len(min_value_total)==0:
+            print('没有最小值')
+            exit(2)
+        if len(com)==0:
+            # min_key_total = min(classes, key=classes.get)
+            id = min_value_total.pop()
+
+        else:
+            # min_key_total = random.choice(com)
+            id = com.pop()
 
         # 获取字典中的最小值对应的键
         # min_key_total = min(classes_total, key=classes_total.get)
         # print(f"最小值对应的键是： {min_key_total}", min_value_total,'最小值')
-        classes[min_key_total].allocate(student)
+        res=classes[id].allocate(student)
+        if res:
+            print(f"学生 {student} 分配到班级 {id} 成功")
+        else:
+            print(f"学生 {student} 分配到班级 {id} 失败")
+            studic.append(student)
+
 
 
 
