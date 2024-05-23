213b3d23b3a53c8d74a3f5eb7a41949a33f60afa
wangmin <mingkingwm@gmail.com>
2024-05-23 10:31:55 +0800

    feat: 增加认证与缓存相关逻辑

--- views/school/router.py
+++ views/school/router.py
@@ -1,18 +1,13 @@
 from mini_framework.web.router import Router
 
-from views.models.school import School
-from views.models.test import ApplicationInfo
+from views.school.campus_view import CampusView
 from views.school.classes_view import ClassesView
 from views.school.course_view import CourseView
+from views.school.institution_view import InstitutionView
 from views.school.leader_info_view import LeaderInfoView
 from views.school.major_view import MajorView
-from views.school.school_view import SchoolView
-from views.tests.test_view import TestView
-from views.models.planning_school import PlanningSchool
 from views.school.planning_school_view import PlanningSchoolView
-from views.models.institutions import Institutions
-from views.school.institution_view import InstitutionView
-from views.school.campus_view import CampusView
+from views.school.school_view import SchoolView
 
 
 def routers():
