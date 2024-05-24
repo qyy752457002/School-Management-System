6fc278ba93a707887e65b62590a035bc0b2f35d0
xuejiajia <xjj1987@qq.com>
2024-05-24 18:17:33 +0800

    删除SQL

--- sql/lfun_enum_value_202404191950.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.lfun_enum_value (enum_name,enum_value,description,sort_number,parent_id,is_enabled,created_uid,updated_uid,created_at,updated_at,is_deleted) VALUES
-	 ('fouder_type','地方',NULL,NULL,'0',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
-	 ('fouder_type','中央',NULL,NULL,'0',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
-	 ('fouder_type_lv2','民办',NULL,NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
-	 ('fouder_type_lv2','教育部门',NULL,NULL,'2',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
-	 ('fouder_type_lv3','民办',NULL,NULL,'4',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false),
-	 ('fouder_type_lv3','省级教育部门',NULL,NULL,'5',true,0,0,'2020-10-01 00:00:00','2020-01-01 00:00:00',false);
