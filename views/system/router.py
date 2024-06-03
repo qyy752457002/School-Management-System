76e95450dc3a99596f8696eb58f917078a3e115e
xuejiajia <xjj1987@qq.com>
2024-06-03 14:55:36 +0800

    获取菜单的接口

--- views/system/router.py
+++ views/system/router.py
@@ -5,6 +5,7 @@ from views.system.enum_value_view import EnumValueView
 from views.system.operation_record_view import OperationRecordView
 from views.system.storage_view import StorageView
 from views.system.sub_system_view import SubSystemView
+from views.system.system_view import SystemView
 
 
 def routers():
@@ -12,6 +13,7 @@ def routers():
     router.include_api_view_class(OperationRecordView, "/v1/system", description="操作日志管理")
 
     router.include_api_view_class(SubSystemView, "/v1/subsystem", description="子系统列表")
+    router.include_api_view_class(SystemView, "/v1/system", description="系统模块")
 
     router.include_api_view_class(EnumValueView, "/v1/enums", description="枚举值列表")
 
