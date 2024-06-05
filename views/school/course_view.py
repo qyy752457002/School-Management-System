d1ee24293c5e198a75c438ec7b8b3ed2f95e4ad3
xuejiajia <xjj1987@qq.com>
2024-06-05 14:41:04 +0800

    学校的课程是默认还是采用复制市的 待定需求

--- views/school/course_view.py
+++ views/school/course_view.py
@@ -3,8 +3,10 @@ from typing import List
 from mini_framework.design_patterns.depend_inject import get_injector
 from mini_framework.web.std_models.page import PageRequest, PaginatedResponse
 from mini_framework.web.views import BaseView
+from starlette.requests import Request
 
 from rules.course_rule import CourseRule
+from views.common.common_view import get_extend_params
 # from views.models.courses import Courses
 from views.models.course import Course
 
@@ -15,20 +17,29 @@ class CourseView(BaseView):
     def __init__(self):
         super().__init__()
         self.course_rule = get_injector( CourseRule)
-
+    # 新增课程  市区  校
     async def post(self,
+                   request:Request,
+
                    school_id:int= Query(...,   description="学校ID", example='1'),
                    course_list:List[Course]= Body([], description="选择的课程" , example= [{"course_id":1,"course_name":"语文","course_no":"19","school_id":1,} ]),
 
                    ):
         # print(course)
+        obj= await get_extend_params(request)
+        for course in course_list:
+
+            course.city = obj.city
+            course.district = obj.county_id
+            if obj.school_id:
+                course.school_id = int(obj.school_id)
         res =await self.course_rule.add_course_school(school_id,course_list )
 
         return res
 
 
 
-
+    # 分页
 
     async def page(self,
                    page_request= Depends(PageRequest),
@@ -58,7 +69,7 @@ class CourseView(BaseView):
 
         return  res
 
-    # 修改 关键信息
+    # 修改 当前用的这个  学校的课程选择 的变更
     async def put(self,
         school_id:int= Query(...,   description="学校ID", example='1'),
         course_list:List[Course]= Body([], description="选择的课程" , example= [{"course_id":1,"course_name":"语文","course_no":"19","school_id":1,} ]),
@@ -75,37 +86,37 @@ class CourseView(BaseView):
 
 
     # 获取所有的课程列表 给下拉
-    async def get_all(self ):
-        # print(page_request)
-        items=[]
-
-
-        res = await self.course_rule.get_course_all( {'school_id':0} )
-        return res
-
-
-
-    async def post_add_init_course(self, course: Course):
-        print(course)
-        res =await self.course_rule.add_course(course)
-
-        return res
-
-
-    # 删除
-    async def delete_init_course(self, course_id:int= Query(..., title="", description="课程id", example='SC2032633'),):
-        print(course_id)
-        # return  course_id
-        res = await self.course_rule.softdelete_course(course_id)
-
-        return  res
-
-    # 修改 关键信息
-    async def put_init_course(self,course:Course
-                  ):
-        # print(planning_school)
-        # todo 记录操作日志到表   参数发进去   暂存 就 如果有 则更新  无则插入
-        res = await self.course_rule.update_course(course)
-
-
-        return  res
+    # async def get_all(self ):
+    #     # print(page_request)
+    #     items=[]
+    #
+    #
+    #     res = await self.course_rule.get_course_all( {'school_id':0} )
+    #     return res
+    #
+    #
+    #
+    # async def post_add_init_course(self, course: Course):
+    #     print(course)
+    #     res =await self.course_rule.add_course(course)
+    #
+    #     return res
+    #
+    #
+    # # 删除
+    # async def delete_init_course(self, course_id:int= Query(..., title="", description="课程id", example='SC2032633'),):
+    #     print(course_id)
+    #     # return  course_id
+    #     res = await self.course_rule.softdelete_course(course_id)
+    #
+    #     return  res
+    #
+    # # 修改 关键信息
+    # async def put_init_course(self,course:Course
+    #               ):
+    #     # print(planning_school)
+    #     # todo 记录操作日志到表   参数发进去   暂存 就 如果有 则更新  无则插入
+    #     res = await self.course_rule.update_course(course)
+    #
+    #
+    #     return  res
