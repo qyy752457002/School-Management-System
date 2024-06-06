5d8a029351ac9584a8527362da8455230d28eb32
xuejiajia <xjj1987@qq.com>
2024-06-06 16:04:20 +0800

    加解析文件返回json接口

--- views/system/storage_view.py
+++ views/system/storage_view.py
@@ -17,3 +17,9 @@ class StorageView(BaseView):
 
     async def get_teacher_upload_uri(self, filename: str, file_size: int):
         return await self._storage_rule.get_upload_teacher_info_token_uri(filename, file_size)
+
+#     解析 文件和桶  返回 数据结构
+    async def get_file_data_preview(self, filename: str, bucket,sence=''):
+        return await self._storage_rule.get_file_data_preview(filename, bucket,sence)
+
+
