783015ed656fde1899d8eaff0524d36eb6fb64cd
chengjiru <chengjiru@12345>
2024-06-11 15:08:33 +0800

    feat:增加教师基本信息字段

--- models/teachers_info.py
+++ models/teachers_info.py
@@ -114,5 +114,4 @@ class TeacherInfo(BaseDBModel):
     approval_status: Mapped[str] = mapped_column(String(255), nullable=False, comment="审批状态",
                                                  default="submitting")
     department: Mapped[str] = mapped_column(String(255), nullable=True, default="", comment="部门")
-    # username: Mapped[str] = mapped_column(String(255), nullable=False, comment="用户名")
-    # hash_password: Mapped[str] = mapped_column(String(128), nullable=False, comment="密码")
+
