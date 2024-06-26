b3dbb91b2e995cc18baba8169a0342b8defdb996
chengjiru <chengjiru@12345>
2024-06-26 11:32:04 +0800

    feat：修改操作日志内容

--- views/models/operation_record.py
+++ views/models/operation_record.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
 from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
 
 
-class OperationTargetType(str, Enum):
+class OperationTarget(str, Enum):
     """
     操作主体类型
 
@@ -34,32 +36,45 @@ class OperationType(str, Enum):
         return [cls.CREATE, cls.DELETE, cls.MODIFY, ]
 
 
-class OperationModule(str, Enum):
+class ChangeModule(str, Enum):
     """
     操作模块
-
+    创建学校：create_school
+    入职：new_entry
+    基本信息变更：basic_info_change
+    关键信息变更：key_info_change
+    调动：transfer
+    变动：transaction
+    借动：borrow
+    离退休：retirement
     """
-    CREATE_SCHOOL = "开园"
-    KEYINFO  = "关键信息"
-    BASEINFO  = "基础信息"
-    FAMILYINFO  = "家庭成员信息"
+    CREATE_SCHOOL = "create_school"
+    NEW_ENTRY = "new_entry"
+    BASIC_INFO_CHANGE = "basic_info_change"
+    KEY_INFO_CHANGE = "key_info_change"
+    TRANSFER = "transfer"
+    TRANSACTION = "transaction"
+    BORROW = "borrow"
+    RETIREMENT = "retirement"
+
     @classmethod
     def to_list(cls):
-        return [cls.CREATE_SCHOOL, cls.KEYINFO, cls.BASEINFO, ]
+        return [cls.CREATE_SCHOOL, cls.NEW_ENTRY, cls.BASIC_INFO_CHANGE, cls.KEY_INFO_CHANGE,
+                cls.TRANSFER, cls.TRANSACTION, cls.BORROW,
+                cls.RETIREMENT]
 
 
 class OperationRecord(BaseModel):
-    action_target_id: str = Field(..., title="操作对象ID", description="操作对象ID", examples=[''])
+    action_target_id: int = Field(..., title="操作对象ID", description="操作对象ID", examples=[''])
+    target: OperationTarget = Field(..., description=" 操作对象", examples=[''])
     action_type: str = Field(..., title="操作类型", description="操作类型", examples=[''])
     ip: str = Field(..., title=" Description", description="操作IP", examples=[''])
-    change_data: str = Field(..., title=" Author", description="变更前后数据", examples=[''])
-    change_field: str = Field(..., description=" 变更字段", examples=[''])
-    change_item: str = Field(..., description=" 变更项", examples=[''])
-    timestamp: str = Field(..., description="操作时间 ", examples=[''])
-    action_reason: str = Field(..., description=" 操作原因", examples=[''])
+    change_data: Optional[str] = Field("", title=" Author", description="变更前后数据", examples=[''])
+    operation_time: datetime = Field(datetime.now(), description="操作时间", examples=[''])
     doc_upload: str = Field(..., description=" 附件", examples=[''])
+    change_module: ChangeModule = Field(..., description=" 变更模块", examples=[''])
+    change_detail: str = Field(..., description=" 变更详情", examples=[''])
     status: str = Field(..., description=" 状态", examples=[''])
-    account: str = Field(..., description=" 操作账号", examples=[''])
-    operator: str = Field(..., description=" 操作人", examples=[''])
-    module: str = Field(..., description=" 操作模块", examples=[''])
-    target: str = Field(..., description=" 操作对象", examples=[''])
+    operator_name: str = Field(..., description=" 操作账号", examples=[''])
+    operator_id: int = Field(0, description=" 操作人", examples=[''])
+    process_instance_id: Optional[int] = Field(None, description="流程ID", examples=[''])
