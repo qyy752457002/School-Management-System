6403a139b40e0ea3b7a2144cb484a36ee80365e5
xuejiajia <xjj1987@qq.com>
2024-06-27 11:02:05 +0800

    '

--- models/teacher_transaction.py
+++ models/teacher_transaction.py
@@ -27,7 +27,7 @@ class TeacherTransaction(BaseDBModel):
     transaction_remark: Mapped[str] = mapped_column(String(255), nullable=True, comment="备注", default='')
     original_position: Mapped[str] = mapped_column(String(64), nullable=True,default='', comment="原任职岗位")
     current_position: Mapped[str] = mapped_column(String(64), nullable=True,default='', comment="现任职岗位")
-    position_date: Mapped[date] = mapped_column(Date, nullable=True,default=date(1970, 1, 1), comment="任职日期")
+    position_date: Mapped[date] = mapped_column(Date, nullable=True,default= None, comment="任职日期")
     transaction_time: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nullable=False,
                                                        comment="操作时间")
     operator_name: Mapped[str] = mapped_column(String(255), nullable=False, comment="操作人", default='')
