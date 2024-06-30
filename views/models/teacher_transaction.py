4cb486f454491acc879919213645d79155fa2469
chengjiru <chengjiru@12345>
2024-06-30 16:13:09 +0800

    feat:增加老师异动相关

--- views/models/teacher_transaction.py
+++ views/models/teacher_transaction.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, model_validator, ValidationError, field_validator
+from pydantic import BaseModel, Field, model_validator
 from fastapi import Query
 from datetime import date, datetime
 
