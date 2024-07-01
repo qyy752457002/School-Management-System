7783b79bb70e595023e428d5815754d3185500ce
xuejiajia <xjj1987@qq.com>
2024-07-01 15:26:42 +0800

    feat 学校的草稿状态不允许审批发起

--- business_exceptions/school.py
+++ business_exceptions/school.py
@@ -13,3 +13,6 @@ class SchoolBaseInfoValidateError(BaseException ):
     def __init__(self):
         super().__init__(404, "SCHOOL_VALIDATE_FAILED", "School validae failed.", "缺乏必要参数|请检查参数")
 
+class SchoolStatusError(MiniHTTPException):
+    def __init__(self):
+        super().__init__(404, "SCHOOL_STATUS_ERROR", "School Status Error .", "学校当前的状态不支持此操作,请检查状态")
