4396895fdd0e0b9771e03b7d1218f209f8f98ab0
xuejiajia <xjj1987@qq.com>
2024-07-02 15:35:14 +0800

    feat 增加事业单位的详情接口

--- rules/school_rule.py
+++ rules/school_rule.py
@@ -46,19 +46,11 @@ class SchoolRule(object):
 
     async def get_school_by_id(self, school_id,extra_model=None):
         school_db = await self.school_dao.get_school_by_id(school_id)
-        # 可选 , exclude=[""]
         if not school_db:
             return None
         if extra_model:
             school = orm_model_to_view_model(school_db, extra_model)
-
-            # planning_school_extra = orm_model_to_view_model(planning_school_db, extra_model,
-            #                                                 exclude=[""])
-            # return planning_school,planning_school_extra
-
         else:
-
-            # return planning_school
             school = orm_model_to_view_model(school_db, SchoolModel)
         return school
 
