5ded9c74ff508ff65ef834fb2cddd322a0042270
xuejiajia <xjj1987@qq.com>
2024-07-15 15:17:57 +0800

    fix 加log

--- views/tasks/planning_school_task.py
+++ views/tasks/planning_school_task.py
@@ -33,9 +33,9 @@ class PlanningSchoolExecutor(TaskExecutor):
             data= [ ]
             # 得到的是下载链接  下载到本地
             fileinfo =await self.system_rule.get_download_url_by_id(info.file_name)
-            logger.debug( f"{fileinfo}",  )
+            logger.debug('根据ID下载文件', f"{fileinfo}",  )
             data =await self._storage_rule.get_file_data(info.file_name, info.bucket_name,info.scene,file_direct_url=fileinfo)
-            logger.debug( f"{data}",  )
+            logger.debug('根据URL解析数据', f"{data}",  )
 
             for item in data:
 
@@ -48,6 +48,8 @@ class PlanningSchoolExecutor(TaskExecutor):
                     raise ValueError("Invalid payload type")
                 res = await self.planning_school_rule.add_planning_school(data_import)
                 print('得到的结果视图模型 ', res, '模型1')
+                logger.debug('得到的结果视图模型', f"{res}",  )
+
                 # 解析 拆分到第二个模型
                 data_import.id =    0
 
@@ -56,6 +58,7 @@ class PlanningSchoolExecutor(TaskExecutor):
                 resc.id= 0
                 newid = str(res.id)
                 print(resc, '模型23', res.id, type(res.id))
+                logger.debug('得到的结果视图模型 communication', f"{resc}",  )
 
                 resc.planning_school_id = int(newid)
                 print(resc, newid, '||||||||')
@@ -64,17 +67,19 @@ class PlanningSchoolExecutor(TaskExecutor):
                 res_comm = await self.planning_school_communication_rule.add_planning_school_communication(resc,
                                                                                                            convertmodel=True)
                 print(res_comm, '模型2 res')
+                logger.debug('得到的结果视图模型 communication', f"{res_comm}",  )
+
                 task.result_file =  ''
                 task.result_bucket =  ''
 
                 print('插入数据res',res)
                 logger.debug( f"{res}",  )
 
-            logger.info(f"任务   created")
+            logger.info(f"任务   success")
         except Exception as e:
             traceback.print_exc()
             print(e,'异常')
-            logger.debug( f"任务   create failed", traceback.format_exception(e))
+            logger.debug( f"任务   执行 failed", traceback.format_exception(e))
 
             # logger.error(f"任务   create failed")
 
@@ -88,19 +93,26 @@ class PlanningSchoolExportExecutor(TaskExecutor):
         self.planning_school_communication_rule = get_injector(PlanningSchoolCommunicationRule)
         super().__init__()
     async def execute(self, task: 'Task'):
-        print("test")
-        print(dict(task))
-        task: Task = task
-        logger.info("负载" ,task.payload)
-        if isinstance(task.payload, dict):
-            student_export: PlanningSchoolPageSearch = PlanningSchoolPageSearch(**task.payload)
-        elif isinstance(task.payload, PlanningSchoolPageSearch):
-            student_export: PlanningSchoolPageSearch = task.payload
-        else:
-            raise ValueError("Invalid payload type")
-        task_result = await self.planning_school_rule.planning_school_export(task)
-        task.result_file = task_result.result_file
-        task.result_bucket = task_result.result_bucket
+        try:
+            print("test")
+            print(dict(task))
+            task: Task = task
+            logger.info("负载的数据" ,task.payload)
+            if isinstance(task.payload, dict):
+                student_export: PlanningSchoolPageSearch = PlanningSchoolPageSearch(**task.payload)
+            elif isinstance(task.payload, PlanningSchoolPageSearch):
+                student_export: PlanningSchoolPageSearch = task.payload
+            else:
+                raise ValueError("Invalid payload type")
+            task_result = await self.planning_school_rule.planning_school_export(task)
+            task.result_file = task_result.result_file
+            task.result_bucket = task_result.result_bucket
+            logger.debug("导入规划校的结果" ,task)
+        except Exception as e:
+            traceback.print_exc()
+            logger.debug( f"任务   exe failed", traceback.format_exception(e))
+
+
 
         # task.result_file =  ''
         # task.result_bucket =  ''
