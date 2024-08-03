31520ecbe8cc7c6217d70a020af9732783b69dd6
xuejiajia <xjj1987@qq.com>
2024-08-03 16:23:42 +0800

    fix  移除多余的引用

--- daos/student_temporary_study_dao.py
+++ daos/student_temporary_study_dao.py
@@ -2,7 +2,6 @@ from sqlalchemy import select, func, update
 from mini_framework.databases.entities.dao_base import DAOBase, get_update_contents
 from mini_framework.databases.queries.pages import Paging
 from mini_framework.web.std_models.page import PageRequest
-from sqlalchemy_utils.functions.orm import is_deleted
 
 from models.student_temporary_study import StudentTemporaryStudy
 
