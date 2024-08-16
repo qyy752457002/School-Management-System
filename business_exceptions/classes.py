6736141fa92b1fa50a745e31720af4bf0ee34c39
xuejiajia <xjj1987@qq.com>
2024-08-16 17:59:00 +0800

    fix 加删除和编辑时的校验

--- business_exceptions/classes.py
+++ business_exceptions/classes.py
@@ -10,3 +10,7 @@ class ClassesAlreadyExistError(MiniHTTPException):
     def __init__(self):
         super().__init__(404, "CLASSES_ALREADY_EXIST", "Classes already exist.", "班级已存在")
 
+
+class ClassesLockedError(MiniHTTPException):
+    def __init__(self):
+        super().__init__(404, "CLASSES_ALREADY_LOCKED", "Classes already locked.", "班级已经被锁定,暂时无法操作")
