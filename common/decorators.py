4ef6d5ae31e3c93377c0555db3e1b910cf9b95ff
chengjiru <chengjiru@12345>
2024-08-21 18:24:24 +0800

    feat:修改授权关闭装饰器验证

--- common/decorators.py
+++ common/decorators.py
@@ -8,9 +8,9 @@ def require_role_permission(role: str, action: str):
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
-            v = await verify_auth_by_obj_and_act(role, action)
-            if not v:
-                raise NoPermissionError()
+            # v = await verify_auth_by_obj_and_act(role, action)
+            # if not v:
+            #     raise NoPermissionError()
             return await func(*args, **kwargs)
         return wrapper
     return decorator
