7857ba3a7804e08d94c95428c10458efc07fa348
xuejiajia <xjj1987@qq.com>
2024-08-22 18:42:46 +0800

    fix 加权限开关的配置项/负责校验和拦截菜单

--- common/decorators.py
+++ common/decorators.py
@@ -4,21 +4,21 @@ from mini_framework.web.request_context import request_context_manager
 
 from business_exceptions.auth import NoPermissionError
 from rules.common.common_rule import verify_auth_by_obj_and_act
+from views.common.common_view import system_config
 
 
 def require_role_permission(role: str, action: str):
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
-            # print(args, )
-            # print( kwargs)
-
-            # print('tt', type(token),token )
-
             v = await verify_auth_by_obj_and_act(role, action)
+            is_permission_verify = system_config.system_config.get("permission_verify")
+            print('permission verify',  is_permission_verify)
+
             if not v:
-                print('no permission', role, action)
-                raise NoPermissionError()
+                print('no permission', role, action,)
+                if is_permission_verify:
+                    raise NoPermissionError()
             return await func(*args, **kwargs)
         return wrapper
     return decorator
