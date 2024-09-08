e44371b2f4761f1251611c734162b5d470273390
xuejiajia <xjj1987@qq.com>
2024-09-08 15:32:48 +0800

    fix  重复提示  自动显示行政事业

--- business_exceptions/institution.py
+++ business_exceptions/institution.py
@@ -7,3 +7,6 @@ class InstitutionNotFoundError(MiniHTTPException):
 class InstitutionStatusError(MiniHTTPException):
     def __init__(self):
         super().__init__(404, "INSTITUTION_STATUS_ERROR", "Institution Status Error .", "事业/行政单位当前的状态不支持此操作,请检查状态")
+class InstitutionExistError(MiniHTTPException):
+    def __init__(self):
+        super().__init__(404, "INSTITUTION_EXIST", "Institution EXIST.", "事业/行政单位已存在")
