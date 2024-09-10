827040a4cc0b8aaf50e69410cfd5224aa97e6a24
xuejiajia <xjj1987@qq.com>
2024-09-10 11:10:21 +0800

    fix  索引

---
+++ sql/索引.sql
@@ -0,0 +1,10 @@
+CREATE INDEX "block" ON "public"."lfun_planning_school" (
+  "block"
+);
+
+CREATE INDEX "block" ON "public"."lfun_school" (
+  "block"
+);
+CREATE INDEX "teacher_employer" ON "public"."lfun_teachers" (
+  "teacher_employer"
+);
